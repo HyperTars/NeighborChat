71a1566964c13abf958395311ce1055a99d0b3aa
mike yan <mikeyan@mikeyan.cable.rcn.com>
2019-12-18 16:09:08 -0500

    modification

--- src/main/java/com/hypertars/neighborChat/web/Controller/UserController.java
+++ src/main/java/com/hypertars/neighborChat/web/Controller/UserController.java
@@ -7,7 +7,6 @@ import com.hypertars.neighborChat.web.NBCBaseController;
 import com.hypertars.neighborChat.web.NBCLogicCallBack;
 import com.hypertars.neighborChat.web.NBCResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +22,11 @@ public class UserController extends NBCBaseController {
     @Resource
     private UserService userService;
 
-    @GetMapping("loginIn")
+    @RequestMapping(value = "loginIn", produces = "text/script;charset=UTF-8")
 //    @RequestMapping()
 //    @PostMapping
     /* @RequestMapping */
-    public String loginIn(HttpServletRequest request, HttpServletResponse response) {
+    public String loginIn(HttpServletRequest request, HttpServletResponse response, String callback) {
         String uname = request.getParameter("uname");
         String pass = request.getParameter("pass");
 
@@ -41,7 +40,8 @@ public class UserController extends NBCBaseController {
         response.addCookie(cookie);
 
         user = userService.getUserBySession(session);
-        return JSON.toJSONString(user);
+        return callback + "(" + JSON.toJSONString(user) + ")";
+//        return JSON.toJSONString(user);
     }
 
     @GetMapping("getUserInfo")
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,31 @@
+$("#form").submit(function (e) {
+    var InputName = $('#InputName').val();
+    var InputPassword = $('#InputPassword').val();
+
+    if (InputName == "" || InputPassword == "") {
+        alert("Username or password cannot be null");
+        e.preventDefault();
+        return;
+    }
+
+    var jsonObj = {
+        "uname": InputName,
+        "pass": InputPassword,
+    };
+
+    $.ajax({
+        type: 'get',
+        dataType: "jsonp",
+        jsonp: "callback", 
+        data: jsonObj,
+        url: "http://localhost:8084/user/loginIn?uname=" + InputName + "&pass" + InputPassword,
+        success: function(callback) {
+            console.log(callback);
+            window.location.href ="dashboard.html";
+        },
+        error: function() {
+            alert("No such user, or wrong password!");
+        }
+    });
+    e.preventDefault();
+})
